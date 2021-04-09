855ae365ecebab1c775f272925be2f7a9ee78058
jrandtc <tc10815p@pace.edu>
2021-04-09 16:00:29 -0400

    just added a few empty classes for teammates

---
+++ UnofficialBronxZooAudioTourGuide/app/src/main/java/com/cs639/unofficialbronxzooaudiotourguide/AnimalContainerActivity.java
@@ -0,0 +1,14 @@
+package com.cs639.unofficialbronxzooaudiotourguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnimalContainerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animal_container);
+    }
+}

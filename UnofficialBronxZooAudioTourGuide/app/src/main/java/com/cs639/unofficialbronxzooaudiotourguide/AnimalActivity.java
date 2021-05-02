a9837da395623360cf9d1a2daad9dfbad4719138
tc10815 <twcookson@gmail.com>
2021-05-02 18:56:34 -0400

    all code written by authors other than Tom Cookson removed

--- UnofficialBronxZooAudioTourGuide/app/src/main/java/com/cs639/unofficialbronxzooaudiotourguide/AnimalActivity.java
+++ UnofficialBronxZooAudioTourGuide/app/src/main/java/com/cs639/unofficialbronxzooaudiotourguide/AnimalActivity.java
@@ -8,16 +8,12 @@ import android.util.Log;
 import android.widget.TextView;
 
 /**
- * This activity displays all xml information about an animal, provides a picture, provides
- * audio, and offers links to both wikipedia and EOL pages on the animal.
+ * Starting from scratch
  *
- * @author Rucha
+ * @author Tom
  */
 public class AnimalActivity extends AppCompatActivity {
-    private Animal myAnimal;
-    private TextView txtZooName;
-    private TextView txtBinom;
-    private TextView txtDesc;
+
 
     public AnimalActivity() {
     }
@@ -25,21 +21,5 @@ public class AnimalActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_animal_screen);
-        XMLDataGetter myData = new XMLDataGetter(this);
-
-        //get the current intent
-        Intent intent = getIntent();
-        int animalNumber = intent.getIntExtra("animalnumber", 0);
-       
-        myAnimal = myData.getAnimals().get(animalNumber);
-        txtZooName = findViewById(R.id.txtAnimalZooName);
-        txtBinom = findViewById(R.id.txtAnimalBinom);
-        txtDesc = findViewById(R.id.txtAnimalDesc);
-
-        txtZooName.setText(myAnimal.getZooName());
-        txtBinom.setText(myAnimal.getBinomialNomenclature());
-        txtDesc.setText(myAnimal.getDescription().get(0));
-   }
-
+    }
 }
